b1572c98f9405dee8f4e3887794bb809c55fb34d
Karol Galera <frontend.galera@gmail.com>
2023-05-26 20:39:11 +0200

    typo corrected

--- src/components/DishForm/dishValidationSchema.js
+++ src/components/DishForm/dishValidationSchema.js
@@ -7,13 +7,13 @@ export const dishValidationSchema = Yup.object({
       /^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/,
       "Invalid format. Please use HH:MM:SS"
     )
-    .required("FIeld is required"),
+    .required("Field is required"),
   type: Yup.string().required("Field is required"),
   no_of_slices: Yup.number().when("type", {
     is: "pizza",
     then: () =>
       Yup.number()
-        .min(2, "It could be at least 2 slices")
+        .min(2, "It should be at least 2 slices")
         .max(10, "It could be a max of 10 slices")
         .required("Field is required"),
   }),
@@ -21,23 +21,23 @@ export const dishValidationSchema = Yup.object({
     is: "pizza",
     then: () =>
       Yup.number()
-        .min(20.0, "It could be at least 20.00")
+        .min(20.0, "It should be at least 20.00")
         .max(60.0, "It could be max of 60.00")
-        .required("Filed is required"),
+        .required("Field is required"),
   }),
   spiciness_scale: Yup.number().when("type", {
     is: "soup",
     then: () =>
       Yup.number()
-        .min(1, "Scale must be at least 1")
-        .max(10, "Scale coold be a mx of 10")
-        .required("FIeld is required"),
+        .min(1, "Scale should at least 1")
+        .max(10, "Scale could be a max of 10")
+        .required("Field is required"),
   }),
   slices_of_bread: Yup.number().when("type", {
     is: "sandwich",
     then: () =>
       Yup.number()
-        .min(1, "It could be at least 1 slice")
+        .min(1, "It should be at least 1 slice")
         .max(10, "It could be max of 10 slices")
         .required("Field is required"),
   }),
