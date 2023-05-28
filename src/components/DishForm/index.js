224638b8415fc4f6262c053bee73db95dfc586eb
Karol Galera <frontend.galera@gmail.com>
2023-05-28 22:52:22 +0200

    code and styles refactoring in DishForm component

--- src/components/DishForm/index.js
+++ src/components/DishForm/index.js
@@ -51,38 +51,35 @@ const DishForm = () => {
   return (
     <>
       <Form onSubmit={formik.handleSubmit}>
-        <div>
-          <TextInput
-            label="Dish name*:"
-            name="name"
-            type="text"
-            formik={formik}
-          />
-        </div>
-        <div>
-          <TextInput
-            label="Preparation time*:"
-            name="preparation_time"
-            type="text"
-            placeholder="HH:MM:SS"
-            formik={formik}
-          />
-        </div>
-        <div>
-          <SelectInput
-            label="Type of dish*:"
-            name="type"
-            options={[
-              { value: "", text: "" },
-              { value: "pizza", text: "Pizza" },
-              { value: "soup", text: "Soup" },
-              { value: "sandwich", text: "Sandwich" },
-            ]}
-            formik={formik}
-          />
-        </div>
+        <TextInput
+          label="Dish name*:"
+          name="name"
+          type="text"
+          formik={formik}
+        />
+
+        <TextInput
+          label="Preparation time*:"
+          name="preparation_time"
+          type="text"
+          placeholder="HH:MM:SS"
+          formik={formik}
+        />
+
+        <SelectInput
+          label="Type of dish*:"
+          name="type"
+          options={[
+            { value: "", text: "" },
+            { value: "pizza", text: "Pizza" },
+            { value: "soup", text: "Soup" },
+            { value: "sandwich", text: "Sandwich" },
+          ]}
+          formik={formik}
+        />
+
         {formik.values.type === "pizza" && (
-          <div>
+          <>
             <TextInput
               label="Number of slices*:"
               name="no_of_slices"
@@ -97,27 +94,23 @@ const DishForm = () => {
               step="0.01"
               formik={formik}
             />
-          </div>
+          </>
         )}
         {formik.values.type === "soup" && (
-          <div>
-            <TextInput
-              label="Spiciness scale (1-10)*:"
-              name="spiciness_scale"
-              type="number"
-              formik={formik}
-            />
-          </div>
+          <TextInput
+            label="Spiciness scale (1-10)*:"
+            name="spiciness_scale"
+            type="number"
+            formik={formik}
+          />
         )}
         {formik.values.type === "sandwich" && (
-          <div>
-            <TextInput
-              label="Slices of bread*:"
-              name="slices_of_bread"
-              type="number"
-              formik={formik}
-            />
-          </div>
+          <TextInput
+            label="Slices of bread*:"
+            name="slices_of_bread"
+            type="number"
+            formik={formik}
+          />
         )}
         <Button type="submit">Submit</Button>
       </Form>
--- src/components/DishForm/styled.js
+++ src/components/DishForm/styled.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
-  flex-basis: 750px;
-  margin: 20px;
+  width: 50%;
+  margin: 0px;
   padding: 20px 40px 20px 40px;
-  background: ${({ theme }) => theme.colors.firefly};
-  border-radius: 30px;
-  box-shadow: 0 0 2px 2px ${({ theme }) => theme.colors.white};
+  background: ${({ theme }) => theme.colors.desertSand};
+  border-top-left-radius: ${({ theme }) => theme.borderRadius.big};
+  border-bottom-left-radius: ${({ theme }) => theme.borderRadius.big};
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    flex-basis: 450px;
     padding: 10px 20px 10px 20px;
   }
 
@@ -19,25 +18,30 @@ export const Form = styled.form`
 `;
 
 export const Button = styled.button`
-  border-radius: 50px;
+  border-radius: ${({ theme }) => theme.borderRadius.large};
   border: none;
-  background: ${({ theme }) => theme.colors.torchRed};
-  font-weight: 700;
-  color: ${({ theme }) => theme.colors.white};
-  padding: 15px 20px 15px 20px;
+  background: linear-gradient(
+    180deg,
+    ${({ theme }) => theme.colors.copperfield} 0%,
+    ${({ theme }) => theme.colors.antiqueBrass} 100%
+  );
+  font-weight: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.black};
+  padding: 15px 20px;
   margin: 30px 0 10px 0;
-  font-size: 20px;
+  font-size: ${({ theme }) => theme.font.size.large};
   width: 100%;
   transition: 0.3s;
+  box-shadow: ${({ theme }) => theme.boxShadow.field};
 
   &:hover {
     cursor: pointer;
-    filter: brightness(80%);
-    box-shadow: 0 0 2px 2px ${({ theme }) => theme.colors.white};
+    transform: scale(1.05);
+    box-shadow: ${({ theme }) => theme.boxShadow.hover};
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     padding: 10px 15px 10px 15px;
-    font-size: 16px;
+    font-size: ${({ theme }) => theme.font.size.big};
   }
 `;
